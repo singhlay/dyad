6904d76444774b711cc02cf0ace19858b5f0a3e6
harshsinghlay <harsh.singhlay@synlabs.io>
2025-04-16 17:51:11 +0530

    changed in viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 This ensures proper routing and reload handling
+
   plugins: [react()],
+  
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
